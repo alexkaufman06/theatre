ebe28db11b5a8a2ab3c37026d1aa5202b35b61b7
Alex Kaufman <alexkaufman06@gmail.com>
2015-02-18 15:09:43 -0800

    Add html

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+var Ticket = {
+  newReleases: ["hobbit", "fifty", "still", "poetry"],
+  oldReleases: ["hunger", "big", "night", "gone" ],
+  expensiveTime: ["8pm", "8:30pm", "9pm", "9:30pm", "10pm", "10:30pm", "11pm"],
+  basePrice: 8,
+  price: function() {
+    if ((this.age < 12) || (this.age > 65)) {
+      this.basePrice -= 2;
+    }
+
+    if (this.newReleases.indexOf(this.movie) !== -1 ) {
+      this.basePrice += 5;
+    }
+
+    if (this.expensiveTime.indexOf(this.time) !== -1 ) {
+      this.basePrice += 5;
+    }
+
+    return this.basePrice;
+  }
+};
--- specs/specs.js
+++ specs/specs.js
@@ -2,7 +2,10 @@ describe("Ticket", function() {
   describe("price()", function() {
     it("gives a price for a movie depending on show time, age, and movie selected", function() {
       var testTicket = Object.create(Ticket);
-      expect(testTicket.price()).to.equal(25)
+      testTicket.age = 21;
+      testTicket.movie = "The Hobbit";
+      testTicket.time = "8pm";
+      expect(testTicket.price()).to.equal(18);
     });
   });
 });
